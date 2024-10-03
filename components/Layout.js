f66a873b9ef2aa9275eb2f8e62898a8cc17553f9
Pedro Verani <pedro.verani@fivelabs.com.br>
2024-10-02 21:55:15 -0300

    adding something

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-gray-800 text-white py-4">
-        <div className="container mx-auto px-4 flex items-center">
-          <Link href="/" className="mr-8">
-            <img src="/images/plinio/plinioMenu.png" alt="Artist Icon" className="w-12 h-12 rounded-full" />
-          </Link>
-          <nav className="flex-grow">
-            <ul className="flex justify-between">
+    <div className="min-h-screen flex flex-col bg-gray-800">
+      <header className="py-2">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <nav className="flex items-center justify-evenly">
+            <Link href="/" className="flex items-center">
+              <Image
+                src="/images/plinio/plinioMenu.png"
+                alt="Artist Icon"
+                width={45}
+                height={48}
+                className="rounded-full"
+              />
+            </Link>
+            <ul className="flex space-x-6 items-center text-white">
               <li><Link href="/" className="hover:text-gray-300">Início</Link></li>
-              <li><Link href="/obras" className="hover:text-gray-300">Obras</Link></li>
+              <li><Link href="/masterpieces" className="hover:text-gray-300">Obras</Link></li>
               <li><Link href="/pinturas" className="hover:text-gray-300">Pinturas</Link></li>
               <li><Link href="/desenhos" className="hover:text-gray-300">Desenhos</Link></li>
-              <li><Link href="/quotes" className="hover:text-gray-300">Quotes</Link></li>
-              <li><Link href="/art-periods" className="hover:text-gray-300">Art Periods</Link></li>
+              <li><Link href="/quotes" className="hover:text-gray-300">Citações</Link></li>
+              <li><Link href="/art-periods" className="hover:text-gray-300">Arte</Link></li>
               <li><Link href="/fun-facts" className="hover:text-gray-300">Fatos</Link></li>
               <li><Link href="/biografia" className="hover:text-gray-300">Biografia</Link></li>
             </ul>
@@ -24,13 +31,13 @@ const Layout = ({ children }) => {
         </div>
       </header>
 
-      <main className="flex-grow">
+      <main className="flex-grow bg-white">
         {children}
       </main>
 
       <footer className="bg-gray-800 text-white py-4">
-        <div className="container mx-auto px-4 text-center">
-          &copy; {new Date().getFullYear()} Your Artist Name. All rights reserved.
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+          &copy; {new Date().getFullYear()} Pablo Picasso Website. All rights reserved.
         </div>
       </footer>
     </div>
